4c3390a251b7917f4aeaef5cd889090af5ca8899
Dmitry Zakharov <zakharov@ie.ibm.com>
2026-01-20 23:42:48 +0000

    send strings

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,19 @@ mod config_from_cli;
 mod config_from_env;
 mod logger;
 mod mcp_workers;
+mod post_result;
 mod stdio_reader;
 mod stdio_writer;
 mod streamer;
-mod post_result;
 
 use crate::config::Config;
 use crate::logger::init_logger;
-use crate::stdio_reader::spawn_reader;
-//use flume::bounded;
 use crate::mcp_workers::spawn_workers;
+use crate::stdio_reader::spawn_reader;
 use crate::stdio_writer::spawn_writer;
+use std::sync::Arc;
 
-
-use mcp_stdio_wrapper::streamer::McpStreamClient;
+use streamer::McpStreamClient;
 use tracing::{debug, info};
 
 #[tokio::main]
@@ -28,7 +27,7 @@ async fn main() {
 
     info!("Start");
 
-    let mcp_client = McpStreamClient::new(config.mcp_server_url);
+    let mcp_client = Arc::new(McpStreamClient::new(config.mcp_server_url));
     debug!("Mcp client: {mcp_client:?}");
 
     // (Reader -> Worker)
@@ -37,7 +36,7 @@ async fn main() {
     let (writer_tx, writer_rx) = flume::unbounded::<String>();
 
     spawn_reader(reader_tx);
-    spawn_workers(config.concurrency, &reader_rx, &writer_tx);
+    spawn_workers(config.concurrency, mcp_client, &reader_rx, &writer_tx);
     let exit = spawn_writer(writer_rx);
 
     let _ = exit.await;
--- src/mcp_workers.rs
+++ src/mcp_workers.rs
@@ -1,19 +1,33 @@
+use crate::streamer::McpStreamClient;
 use flume::{Receiver, Sender};
-use tracing::{debug, info};
+use std::sync::Arc;
+use tracing::{debug, error, info};
 
-pub fn spawn_workers(concurrency: usize, input_rx: &Receiver<String>, output_tx: &Sender<String>) {
+pub fn spawn_workers(
+    concurrency: usize,
+    mcp_client: Arc<McpStreamClient>,
+    input_rx: &Receiver<String>,
+    output_tx: &Sender<String>,
+) {
     for i in 0..concurrency {
         let rx = input_rx.clone();
         let tx = output_tx.clone();
+        let client = mcp_client.clone();
 
         tokio::spawn(async move {
             info!("Worker {} started", i);
             while let Ok(line) = rx.recv_async().await {
                 debug!("Worker {i} processing message: {line}");
-
-                let response = line;
-                if tx.send_async(response).await.is_err() {
-                    break;
+                let response = client.stream_post(line).await;
+                match response {
+                    Ok(response) => {
+                        if tx.send_async(response.out).await.is_err() {
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        error!("Post failed: {e}")
+                    }
                 }
             }
             info!("Worker {} shutting down", i);
