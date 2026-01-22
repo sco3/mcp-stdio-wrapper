d955f4b06cf99d90934ead62be4c8d6762e8c5ab
Dima Zakharov <zakharov@ibm.com>
2026-01-22 14:58:31 +0000

    cli works and linter happy

--- tests/test_config.rs
+++ tests/test_config.rs
@@ -1,5 +1,6 @@
 use mcp_stdio_wrapper::config::Config;
-
+/// # Panics
+/// * test failures
 #[tokio::test]
 pub async fn test_config() {
     let _config = Config::from_env();
--- tests/test_logger.rs
+++ tests/test_logger.rs
@@ -1,9 +1,13 @@
 use mcp_stdio_wrapper::logger::init_logger;
+/// # Panics
+/// * test failures
 
 #[tokio::test]
 pub async fn test_logger_init_off() {
     init_logger(Some("off"));
 }
+/// # Panics
+/// * test failures
 
 #[tokio::test]
 pub async fn test_logger_init_info() {
