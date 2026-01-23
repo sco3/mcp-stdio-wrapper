02b4d56641a0cda41a921986d44fba51370ba03a
Dima Zakharov <zakharov@ibm.com>
2026-01-23 15:54:22 +0000

    update docstrings

--- src/json_rpc_header.rs
+++ src/json_rpc_header.rs
@@ -4,7 +4,9 @@ use serde::Deserialize;
 struct JsonRpcHeader {
     id: u64,
 }
-
+/// parses string for jsonrpc id
+/// # Errors
+/// * id not found in input
 pub fn parse_id(json_str: &str) -> Result<u64, Box<dyn std::error::Error>> {
     let header: JsonRpcHeader = serde_json::from_str(json_str)?;
     Ok(header.id)
