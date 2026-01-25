83f75a34731a8f956153e0285b45c3625d41ccb9
Dmitry Zakharov <zakharov@ibm.com>
2026-01-25 13:27:15 +0000

    test termination and eof for reader

--- tests/test_reader.rs
+++ tests/test_reader.rs
@@ -1,3 +1,4 @@
+use mcp_stdio_wrapper::logger::init_logger;
 use mcp_stdio_wrapper::stdio_reader::spawn_reader;
 #[tokio::test]
 ///
@@ -8,31 +9,31 @@ use mcp_stdio_wrapper::stdio_reader::spawn_reader;
 /// # Panics
 ///
 /// Panics if the received line does not match the expected data.
-async fn test_reader_break_coverage() {
-    let (tx, rx) = flume::unbounded::<String>();
+async fn test_reader() {
+    init_logger(Some("debug"));
+    for i in [true, false] {
+        let (tx, rx) = flume::unbounded::<String>();
 
-    // We provide two lines.
-    // Line 1 will be sent successfully.
-    // Line 2 will trigger the send error because we drop(rx) in between.
-    let stdio = tokio_test::io::Builder::new()
-        .read(b"line1\n")
-        .wait(std::time::Duration::from_millis(10)) // Give us time to drop
-        .read(b"line2\n")
-        .build();
+        let stdio = tokio_test::io::Builder::new()
+            .read(b"line1\n")
+            .wait(std::time::Duration::from_millis(10)) // Give us time to drop
+            .read(b"line2\n")
+            .build();
 
-    let handle = spawn_reader(tx, stdio);
+        let handle = spawn_reader(tx, stdio);
 
-    // 1. Receive the first line
-    let first = rx.recv_async().await.expect("Should receive line1");
-    assert_eq!(first, "line1");
+        let first = rx.recv_async().await.expect("Should receive line1");
+        assert_eq!(first, "line1");
 
-    // 2. Kill the receiver
-    drop(rx);
+        if i {
+            // test termination
+            drop(rx);
+        } else {
+            // test eof
+            let first = rx.recv_async().await.expect("Should receive line2");
+            assert_eq!(first, "line2");
+        }
 
-    // 3. Wait for the background task to try to send "line2" and fail
-    let _ = handle.await;
-
-    // After awaiting the handle, the code MUST have executed the break
-    // because that is the only way the task can finish when data is still
-    // available in the reader.
+        let _ = handle.await;
+    }
 }
