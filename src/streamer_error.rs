264cb4a6298fe99ec321958202f7e462cc251656
Dima Zakharov <zakharov@ibm.com>
2026-01-26 11:10:21 +0000

    form a error struct and serialize

--- src/streamer_error.rs
+++ src/streamer_error.rs
@@ -1,7 +1,8 @@
 use crate::json_rpc_header::parse_id;
+use jsonrpc_core::{serde_json, Error, ErrorCode, Failure, Id, Output};
 use tracing::error;
-
-pub fn mcp_error(worker_id: &usize, json_str: &str, error_msg: &str) {
+/// creates error message
+pub fn mcp_error(worker_id: &usize, json_str: &str, error_msg: &str) -> String {
     let id_str = match parse_id(json_str) {
         Ok(id) => {
             if let Some(s) = id.as_str() {
@@ -15,5 +16,18 @@ pub fn mcp_error(worker_id: &usize, json_str: &str, error_msg: &str) {
             "<unknown id>".to_string()
         }
     };
-    error!("Worker {worker_id} rpc id: {id_str} Wrapper: MCP request failed: {error_msg}");
+    let error_obj = Error {
+        code: ErrorCode::InternalError,
+        message: error_msg.to_string(),
+        data: None,
+    };
+
+    let response = Failure {
+        jsonrpc: None, // The serializer will handle the "2.0" versioning
+        error: error_obj,
+        id: Id::Str(id_str),
+    };
+
+    serde_json::to_string_pretty(&response).unwrap()
+    //error!("Worker {worker_id} rpc id: {id_str} Wrapper: MCP request failed: {error_msg}");
 }
