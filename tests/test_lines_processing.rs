e6e5fc5963e9dbe60aee37620f93c807fbc842ce
Dmitry Zakharov <zakharov@ibm.com>
2026-02-04 17:09:21 +0000

    test

--- tests/test_lines_processing.rs
+++ tests/test_lines_processing.rs
@@ -2,6 +2,7 @@ use bytes::{Bytes, BytesMut};
 use mcp_stdio_wrapper::streamer_lines::extract_lines;
 
 /// tests buffer split cr/lf
+/// # Panics on test failure
 #[test]
 pub fn test_process_lines() {
     let mut buffer = BytesMut::from("asdf\r\njkl\n");
