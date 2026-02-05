4e2c2da6ceac7c508c84206a460ed2d3673e855d
Dima Zakharov <zakharov@ibm.com>
2026-02-05 13:19:28 +0000

    token generation

--- token-from-file.sh
+++ token-from-file.sh
@@ -2,13 +2,10 @@
 
 set -ueo pipefail
 
-#export RUST_LOG="reqwest=trace,hyper=trace"
-
 TOKEN_FILE="$HOME/.local/mcpgateway-bearer-token.txt"
 if [ ! -f "$TOKEN_FILE" ]; then
-	echo "Error: Token file not found at $TOKEN_FILE" >&2
-	exit 1
+    AUTH="$(uv --project "${MCP_CONTEXT_FORGE_DIR}" run -m mcpgateway.utils.create_jwt_token --username admin@example.com --exp 10080 --secret my-test-key 2>/dev/null)"
+    echo -n "$AUTH" >$TOKEN_FILE
 fi
 
 AUTH="Bearer $(tr -d '\r\n' <"$TOKEN_FILE")"
-ITERS=1
