e7dcf49072de03ab7e00a0ca221bb48790c60e13
Dmitry Zakharov <zakharov@ibm.com>
2026-02-05 01:01:15 +0000

    two new args

--- src/http_client.rs
+++ src/http_client.rs
@@ -5,13 +5,16 @@ use std::fs::read;
 use std::time::Duration;
 
 pub fn get_http_client(config: &Config) -> Result<Client, String> {
-    let pool_size = config.http_pool_size.unwrap_or(2);
     let mut build = Client::builder()
         .timeout(Duration::from_secs(config.mcp_tool_call_timeout))
         .http2_prior_knowledge()
-        .tcp_nodelay(true)
-        .pool_max_idle_per_host(pool_size)
-        .pool_idle_timeout(Duration::from_secs(90));
+        .tcp_nodelay(true);
+
+    if let Some(pool_size) = config.http_pool_size {
+        build = build.pool_max_idle_per_host(pool_size);
+    }
+
+    build = build.pool_idle_timeout(Duration::from_secs(90));
 
     if let Some(cert_path) = &config.tls_cert {
         let cert_bytes = read(cert_path).map_err(|e| read_error(cert_path, &e))?;
