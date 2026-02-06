3217c30915356491b79920f18e98718efcddb0d5
Dmitry Zakharov <zakharov@ibm.com>
2026-02-06 22:17:58 +0000

    update safely

--- src/streamer_id.rs
+++ src/streamer_id.rs
@@ -5,20 +5,23 @@ impl McpStreamClient {
     /// sets received session id
     pub fn set_session_id(&self, new_id: &str) {
         let mut current_guard = self.session_id.load();
+        let mut current_guard = self.session_id.load();
+        if current_guard.as_deref() == Some(new_id) {
+            return; // no update
+        }
+        let new_arc = Arc::new(Some(new_id.to_string()));
         loop {
-            if current_guard.as_deref() == Some(new_id) {
-                return; // no update
-            }
-            let new_arc = Arc::new(Some(new_id.to_string()));
-
             let prev_guard = self // safe update
                 .session_id
-                .compare_and_swap(&*current_guard, new_arc);
+                .compare_and_swap(&*current_guard, Arc::clone(&new_arc));
 
             if Arc::ptr_eq(&*prev_guard, &*current_guard) {
                 return; // Success
             }
             current_guard = prev_guard;
+            if current_guard.as_deref() == Some(new_id) {
+                return; // another thread set it to the desired value
+            }
         }
     }
 
