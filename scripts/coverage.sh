6731b3945292a64eea75f86b8d434b5b2ae42c7a
Dmitry Zakharov <zakharov@ibm.com>
2026-02-10 11:27:54 +0000

    clean up tests

--- scripts/coverage.sh
+++
@@ -1,6 +0,0 @@
-cargo install cargo-llvm-cov
-rustup component add llvm-tools-preview
-
-time cargo llvm-cov --html --ignore-filename-regex "src/bin/test_stream.rs"
-
-firefox /home/dz/prj/mcp-stdio-wrapper/target/llvm-cov/html/index.html
--- scripts/test-sdk-server-sample.sh
+++ scripts/test-sdk-server-sample.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env -S bash
 
-
-# test for rust-sdk server sample 
+# test for rust-sdk server sample
+# traffic check with tcpdump:
+# sudo tcpdump -i lo -s 0 -w mcp_debug.pcap tcp port 8080
 
 set -ueo pipefail
 
@@ -15,8 +16,8 @@ EXE=(
 )
 
 INIT='{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2024-11-05","capabilities":{},"clientInfo":{"name":"test-client","version":"1.0"}}}'
-NOTIFY='{"jsonrpc":"2.0","id":2, "method":"notifications/initialized"}'
-LIST='{"jsonrpc":"2.0","id":3,"method":"tools/list"}'
+NOTIFY='{"jsonrpc":"2.0","method":"notifications/initialized"}'
+LIST='{"jsonrpc":"2.0","id":2,"method":"tools/list"}'
 
 CALL='{
   "jsonrpc": "2.0",
